ff7507a7fe4b086437a0170d840ad2cb272e0bb8
Kovács Ákos <45033727+kokoakos@users.noreply.github.com>
2023-12-16 14:34:26 +0100

    Add files via upload
    Alpha verzió (v1.0) - Karakter mozgása

---
+++ Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f; // A játékos mozgási sebessége
+
+    // A játék kezdete előtti inicializálás
+    void Start()
+    {
+
+    }
+
+    // A játék folyamán minden képkockában végrehajtandó frissítés
+    [System.Obsolete]
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) // A játékos balra mozgatása
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) // A játékos jobbra mozgatása
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) // A játékos felfelé mozgatása
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) // A játékos lefelé mozgatása
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+        
+    }
+
+}
